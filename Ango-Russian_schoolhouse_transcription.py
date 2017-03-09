0620704d5a8eb18f801964abc2d2a31fe65d7139
Hellerick <hellerickferlibay@gmail.com>
2017-03-09 17:55:10 +0700

    From Severboat

--- Ango-Russian_schoolhouse_transcription.py
+++ Ango-Russian_schoolhouse_transcription.py
@@ -1,8 +1,19 @@
+import os
+
 # rules:
 # https://ru.wikipedia.org/wiki/%D0%90%D0%BD%D0%B3%D0%BB%D0%BE-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B0%D1%8F_%D0%BF%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BA%D1%80%D0%B8%D0%BF%D1%86%D0%B8%D1%8F
 
+project_path = {
+    'DESKTOP-62BVD4A': 'd:\KPV\Github\Ango-Russian_schoolhouse_transcription',
+    'hellerick-C17A': r'/home/hellerick/PycharmProjects/Ango-Russian_schoolhouse_transcription'
+}[platform.node()]
+
+
 def main():
-    pass
+    file_path = os.path.join(
+        project_path, 'Examples',
+        'Eoin Colfer. Artemis Fowl 01.html'
+    )
 
 if __name__ == '__main__':
     main()
